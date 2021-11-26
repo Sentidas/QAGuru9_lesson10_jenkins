647d43a648dc96a55a094272a55c04164e2f96b0
Lena <sentidas@yandex.ru>
2021-11-26 13:07:20 +0300

    done project FillForm for Jenkins

---
+++ src/test/java/fillForm/tests/TestBase.java
@@ -0,0 +1,31 @@
+package fillForm.tests;
+
+import com.codeborne.selenide.Configuration;
+import fillForm.pages.RegistrationPage;
+import helpers.Attach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class TestBase {
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
+}
