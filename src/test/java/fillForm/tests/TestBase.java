184ae5672fde77f5442ff8dc4ffc6ace9796e127
Lena <sentidas@yandex.ru>
2021-11-26 13:29:22 +0300

    added listener

--- src/test/java/fillForm/tests/TestBase.java
+++ src/test/java/fillForm/tests/TestBase.java
@@ -1,8 +1,10 @@
 package fillForm.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import fillForm.pages.RegistrationPage;
 import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -12,6 +14,7 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.startMaximized = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
         DesiredCapabilities capabilities = new DesiredCapabilities();
