8a96df1a21ee70ec600ff19e0c276bfa1c4628cb
Lena <sentidas@yandex.ru>
2021-12-03 13:09:48 +0300

    added owner test

--- src/test/java/fillForm/tests/properties/OwnerTests.java
+++ src/test/java/fillForm/tests/properties/OwnerTests.java
@@ -2,10 +2,11 @@ package fillForm.tests.properties;
 
 import config.CredentialsConfig;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static java.lang.String.format;
-
+@Tag("properties")
 public class OwnerTests {
     public CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class); // фишка owner из документации
 
