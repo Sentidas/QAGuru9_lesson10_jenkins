6f46603c64adf729d34819d9dcd8bbf444e8b571
sentidas <49324420+Sentidas@users.noreply.github.com>
2021-12-03 13:27:45 +0300

    Owner (#1)
    * added owner test

---
+++ src/test/java/fillForm/tests/properties/OwnerTests.java
@@ -0,0 +1,23 @@
+package fillForm.tests.properties;
+
+import config.CredentialsConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static java.lang.String.format;
+@Tag("properties")
+public class OwnerTests {
+    public CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class); // фишка owner из документации
+
+    @Test
+    void readCredentialsTest() {
+        String login = credentials.login();
+        String password = credentials.password();
+
+        System.out.println(login);
+        System.out.println(password);
+        String massage = format("I login as %s with password %s", login, password);
+        System.out.println(massage);
+    }
+}
